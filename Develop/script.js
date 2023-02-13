574a99f115851dd219617c98dc33de9b8ac2cd09
Brian Morse <brian.p.morse@gmail.com>
2023-02-13 14:13:14 -0800

    created functions to return random characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,27 @@
 // Assignment code here
 
 
+// Functions to generate character types in password, calls random ASCII values with ranges determined by number of ASCII characters of a given type plus the position of the first item of a given character type in the ASCII chart.
+function createRandomLowercase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function createRandomUppercase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function createRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+//creates set of symbols to pull from randomly
+
+function createRandomSymbol(){
+  const symbols = "!@#$%^&*(){}[]=<>/";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -14,4 +35,8 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+generateBtn.addEventListener("click", writePassword);{
+
+}

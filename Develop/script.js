fabb5aaa25b163086fed2b5a85b400906b10800a
Brian Morse <brian.p.morse@gmail.com>
2023-02-13 21:55:36 -0800

    completed project

--- Develop/script.js
+++ Develop/script.js
@@ -21,22 +21,80 @@ function createRandomSymbol(){
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+//creates prompts for types of characters to use in password and how many and stores boolean values
+
+
+
+function generatePassword(){
+
+  
+  var lowerBool = confirm("Do you want lowercase characters in your password?");
+
+  var upperBool = confirm("Do you want uppercase characters in your password?");
+  
+  var numberBool = confirm("Do you want numbers in your password?");
+  
+  var symbolsBool = confirm("Do you want lower case characters in your password?");
+  
+  var length = prompt("How many characters would you like your password?");
+
+  while (length < 8 || length > 128) {
+    length = prompt("How many characters would you like your password?");
+  }
+
+  var validChars = []
+
+  var password = ""
+
+  if (lowerBool == true) {
+      validChars.push(createRandomLowercase)
+  }
+
+  if (upperBool == true) {
+    validChars.push(createRandomUppercase)
+}
+  if (numberBool == true) {
+  validChars.push(createRandomNumber)
+}
+  if (symbolsBool == true) {
+  validChars.push(createRandomSymbol)
+}
+
+
+for (let i = 0; i < length; i++){
+  var character = validChars[Math.floor(Math.random() * validChars.length)];
+  
+  password = password + character();
+     }
+return password
+
+}
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+  
+  
+    passwordText.value = password;
 
-  passwordText.value = password;
 
+  
 }
 
 // Add event listener to generate button
 
 
-generateBtn.addEventListener("click", writePassword);{
+generateBtn.addEventListener("click", writePassword);
 
-}
+  
+
